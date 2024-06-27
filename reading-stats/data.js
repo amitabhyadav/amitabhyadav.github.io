6712906a2fb967dd0526e3a08ef82d7790640919
amitabhyadav <amitabhydv@gmail.com>
2024-06-27 22:21:53 +0200

    Update data.js

--- reading-stats/data.js
+++ reading-stats/data.js
@@ -1,6 +1,7 @@
 // Sample reading data
 const readingData = [
-    { date: "2024-06-27", minutes: 0, pages: 0, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "completed", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'hhttps://www.goodreads.com/author/show/22022185.Tony_Fadell' },
+    { date: "2024-06-28", minutes: 0, pages: 0, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "completed", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'hhttps://www.goodreads.com/author/show/22022185.Tony_Fadell' },
+    { date: "2024-06-27", minutes: 25, pages: 12, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'hhttps://www.goodreads.com/author/show/22022185.Tony_Fadell' },
     { date: "2024-06-26", minutes: 40, pages: 26, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "started", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'https://www.goodreads.com/author/show/22022185.Tony_Fadell' },
     // ... more data ...
 ];
