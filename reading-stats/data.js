8326618a36623293ff06f54097be386a1f8d4a2e
amitabhyadav <amitabhydv@gmail.com>
2025-01-03 00:19:41 +0100

    Update data.js

--- reading-stats/data.js
+++ reading-stats/data.js
@@ -1,13 +1,13 @@
 // Sample reading data
 const readingData = [
-    { date: "2025-01-01", minutes: 109, pages: 51, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "started", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
-    { date: "2025-01-02", minutes: 26, pages: 11, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
+    // ... add new data here ...
     //{ date: "2025-01-02", minutes: 26, pages: 11, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "completed", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
-    // ... more data ...
+    { date: "2025-01-02", minutes: 26, pages: 11, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
+    { date: "2025-01-01", minutes: 109, pages: 51, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "started", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
 ];
 
 // Wishlist data
 const wishlistData = [
-    { book: "Guns, Germs and Steel: A Short History of Everybody for the Last 13,000 Years", author: "Jared Diamond", genre: "History" },
+    { book: "Guns, Germs and Steel: A Short History of Everybody for the Last 13,000 Years", author: "Jared Diamond", genre: "History", 'url-book': 'https://www.goodreads.com/book/show/430146.Guns_Germs_and_Steel', 'url-author': 'https://www.goodreads.com/author/show/256.Jared_Diamond'},
     // ... more data ...
 ];
