a65be6d7150bd93f901d73decd626b7149bdcd29
amitabhyadav <amitabhydv@gmail.com>
2025-01-03 00:14:48 +0100

    Update data.js

--- reading-stats/data.js
+++ reading-stats/data.js
@@ -1,7 +1,8 @@
 // Sample reading data
 const readingData = [
-    { date: "2025-01-01", minutes: 109, pages: 51, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
+    { date: "2025-01-01", minutes: 109, pages: 51, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "started", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
     { date: "2024-01-02", minutes: 26, pages: 11, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
+    //{ date: "2024-01-02", minutes: 26, pages: 11, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "completed", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
     // ... more data ...
 ];
 
