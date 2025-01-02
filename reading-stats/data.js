ada300fefe0c61c31984513b405892ee0bac55c6
amitabhyadav <amitabhydv@gmail.com>
2025-01-03 00:13:46 +0100

    Update data.js

--- reading-stats/data.js
+++ reading-stats/data.js
@@ -1,14 +1,12 @@
 // Sample reading data
 const readingData = [
-    { date: "2024-06-29", minutes: 0, pages: 0, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "completed", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'hhttps://www.goodreads.com/author/show/22022185.Tony_Fadell' },
-    { date: "2024-06-28", minutes: 68, pages: 24, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'hhttps://www.goodreads.com/author/show/22022185.Tony_Fadell' },
-    { date: "2024-06-27", minutes: 25, pages: 12, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'hhttps://www.goodreads.com/author/show/22022185.Tony_Fadell' },
-    { date: "2024-06-26", minutes: 40, pages: 26, book: "Build: An Unorthodox Guide to Making Things Worth Making", author: "Tony Fadell", genre: "Enterpreneurship", status: "started", 'url-book': 'https://www.goodreads.com/book/show/59696349-build', 'url-author': 'https://www.goodreads.com/author/show/22022185.Tony_Fadell' },
+    { date: "2025-01-01", minutes: 109, pages: 51, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
+    { date: "2024-01-02", minutes: 26, pages: 11, book: "The Education of an Idealist", author: "Samantha Power", genre: "Memoir", status: "reading", 'url-book': 'https://www.goodreads.com/book/show/42872088-the-education-of-an-idealist', 'url-author': 'https://www.goodreads.com/author/show/13271.Samantha_Power' },
     // ... more data ...
 ];
 
 // Wishlist data
 const wishlistData = [
-    { book: "Determined: A Science of Life without Free Will", author: "Robert M. Sapolsky", genre: "Neuropsychology" },
+    { book: "Guns, Germs and Steel: A Short History of Everybody for the Last 13,000 Years", author: "Jared Diamond", genre: "History" },
     // ... more data ...
 ];
